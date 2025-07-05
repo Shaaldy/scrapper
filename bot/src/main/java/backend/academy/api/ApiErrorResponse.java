7a8272872072635d894619982fdee707990ba1f2
Shaaldy <138975611+Shaaldy@users.noreply.github.com>
2025-07-06 02:06:35 +0500

    update(ApiErrorResponse) - добавлен метод toString для отладки

--- bot/src/main/java/backend/academy/api/ApiErrorResponse.java
+++ bot/src/main/java/backend/academy/api/ApiErrorResponse.java
@@ -1,7 +1,11 @@
 package backend.academy.api;
 
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 public record ApiErrorResponse(String description, String code, String exceptionName, String exceptionMessage,
                                List<String> stackTrace) {
+    public @NotNull String toString() {
+        return description + "\n" + code + "\n" + exceptionName + "\n" + exceptionMessage;
+    }
 }
--- scrapper/src/main/java/backend/academy/scrapper/ApiErrorResponse.java
+++ scrapper/src/main/java/backend/academy/scrapper/ApiErrorResponse.java
@@ -1,8 +1,12 @@
 package backend.academy.scrapper;
 
+import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 
 public record ApiErrorResponse(String description, String code, String exceptionName, String exceptionMessage,
                                ArrayList<String> stackTrace) {
+    public @NotNull String toString() {
+        return description + "\n" + code + "\n" + exceptionName + "\n" + exceptionMessage;
+    }
 }
 
